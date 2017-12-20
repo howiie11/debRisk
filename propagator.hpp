29988e03b723b142416d66be7dfc9294cf1f244f
tQuakes <seapudea@gmail.com>
2017-12-20 01:31:56 -0500

    Commit

--- propagator.hpp
+++ propagator.hpp
@@ -321,92 +321,127 @@ int integrateEoM(double tini,double X0[],double h,int npoints,double duration,
 }
 
 ////////////////////////////////////////////////////////////////////////
-//ATMOSPHERIC MODEL MSIS90E
+//ATMOSPHERIC MODEL NRLMSISE
 ////////////////////////////////////////////////////////////////////////
 /*
-  Source: ftp://ftp.irf.se/pub/perm/ESRAD/SPECTRUM/gtd6.f (JGR, 96, 1159-1172, 1991)
+  Sources:
+    https://www.brodo.de/space/nrlmsise/
+    https://git.linta.de/?p=~brodo/nrlmsise-00.git;a=tree
+
+ *   Switches: to turn on and off particular variations use these switches.
+ *   0 is off, 1 is on, and 2 is main effects off but cross terms on.
+ *
+ *   Standard values are 0 for switch 0 and 1 for switches 1 to 23. The 
+ *   array "switches" needs to be set accordingly by the calling program. 
+ *   The arrays sw and swc are set internally.
+ *
+ *   switches[i]:
+ *    i - explanation
+ *   -----------------
+ *    0 - output in meters and kilograms instead of centimeters and grams
+ *    1 - F10.7 effect on mean
+ *    2 - time independent
+ *    3 - symmetrical annual
+ *    4 - symmetrical semiannual
+ *    5 - asymmetrical annual
+ *    6 - asymmetrical semiannual
+ *    7 - diurnal
+ *    8 - semidiurnal
+ *    9 - daily ap [when this is set to -1 (!) the pointer
+ *                  ap_a in struct nrlmsise_input must
+ *                  point to a struct ap_array]
+ *   10 - all UT/long effects
+ *   11 - longitudinal
+ *   12 - UT and mixed UT/long
+ *   13 - mixed AP/UT/LONG
+ *   14 - terdiurnal
+ *   15 - departures from diffusive equilibrium
+ *   16 - all TINF var
+ *   17 - all TLB var
+ *   18 - all TN1 var
+ *   19 - all S var
+ *   20 - all TN2 var
+ *   21 - all NLB var
+ *   22 - all TN3 var
+ *   23 - turbo scale height var
+
+ * Array containing the following magnetic values:
+ *   0 : daily AP
+ *   1 : 3 hr AP index for current time
+ *   2 : 3 hr AP index for 3 hrs before current time
+ *   3 : 3 hr AP index for 6 hrs before current time
+ *   4 : 3 hr AP index for 9 hrs before current time
+ *   5 : Average of eight 3 hr AP indicies from 12 to 33 hrs 
+ *           prior to current time
+ *   6 : Average of eight 3 hr AP indicies from 36 to 57 hrs 
+ *           prior to current time 
+
+    int year;      / year, currently ignored /
+    int doy;       / day of year /
+    double sec;    / seconds in day (UT) /
+    double alt;    / altitude in kilometers /
+    double g_lat;  / geodetic latitude /
+    double g_long; / geodetic longitude /
+    double lst;    / local apparent solar time (hours), see note below /
+    double f107A;  / 81 day average of F10.7 flux (centered on doy) /
+    double f107;   / daily F10.7 flux for previous day /
+    double ap;     / magnetic index(daily) /
+    struct ap_array *ap_a; / see above /
+
+ *   NOTES ON INPUT VARIABLES: 
+ *      UT, Local Time, and Longitude are used independently in the
+ *      model and are not of equal importance for every situation.  
+ *      For the most physically realistic calculation these three
+ *      variables should be consistent (lst=sec/3600 + g_long/15).
+ *      The Equation of Time departures from the above formula
+ *      for apparent local time can be included if available but
+ *      are of minor importance.
+ *
+ *      f107 and f107A values used to generate the model correspond
+ *      to the 10.7 cm radio flux at the actual distance of the Earth
+ *      from the Sun rather than the radio flux at 1 AU. The following
+ *      site provides both classes of values:
+ *      ftp://ftp.ngdc.noaa.gov/STP/SOLAR_DATA/SOLAR_RADIO/FLUX/
+ *
+ *      f107, f107A, and ap effects are neither large nor well
+ *      established below 80 km and these parameters should be set to
+ *      150., 150., and 4. respectively.
+
+
+ *   OUTPUT VARIABLES:
+ *      d[0] - HE NUMBER DENSITY(CM-3)
+ *      d[1] - O NUMBER DENSITY(CM-3)
+ *      d[2] - N2 NUMBER DENSITY(CM-3)
+ *      d[3] - O2 NUMBER DENSITY(CM-3)
+ *      d[4] - AR NUMBER DENSITY(CM-3)                       
+ *      d[5] - TOTAL MASS DENSITY(GM/CM3) [includes d[8] in td7d]
+ *      d[6] - H NUMBER DENSITY(CM-3)
+ *      d[7] - N NUMBER DENSITY(CM-3)
+ *      d[8] - Anomalous oxygen NUMBER DENSITY(CM-3)
+ *      t[0] - EXOSPHERIC TEMPERATURE
+ *      t[1] - TEMPERATURE AT ALT
+ * 
+ *
+ *      O, H, and N are set to zero below 72.5 km
+ *
+ *      t[0], Exospheric temperature, is set to global average for
+ *      altitudes below 120 km. The 120 km gradient is left at global
+ *      average value for altitudes below 72 km.
+ *
+ *      d[5], TOTAL MASS DENSITY, is NOT the same for subroutines GTD7 
+ *      and GTD7D
+ *
+ *        SUBROUTINE GTD7 -- d[5] is the sum of the mass densities of the
+ *        species labeled by indices 0-4 and 6-7 in output variable d.
+ *        This includes He, O, N2, O2, Ar, H, and N but does NOT include
+ *        anomalous oxygen (species index 8).
+ *
+ *        SUBROUTINE GTD7D -- d[5] is the "effective total mass density
+ *        for drag" and is the sum of the mass densities of all species
+ *        in this model, INCLUDING anomalous oxygen.
+  
+  
  */
-typedef float real;
-real *newVectorf(int n)
-{
-  real *v;
-  v=(real*)malloc(n*sizeof(real));
-  return v;
-}
-extern "C" void gtd6_(int* IYD,real* SEC,real* ALT,real* GLAT,real* GLONG,
-		      real* STL,real* F107A,real* F107,real AP[],int* MASS,
-		      real D[],real T[]);
-/*
-       IYD - YEAR AND DAY AS YYDDD or DDD (day of year from 1 to 365)
-       SEC - UT(SEC)
-       ALT - ALTITUDE(KM)
-       GLAT - GEODETIC LATITUDE(DEG)
-       GLONG - GEODETIC LONGITUDE(DEG)
-       STL - LOCAL APPARENT SOLAR TIME(HRS)
-       AP - MAGNETIC INDEX(DAILY) OR WHEN SW(9)=-1. :
-          - ARRAY CONTAINING:
-	    (1) F107A - 3 MONTH AVERAGE OF F10.7 FLUX
-	    (2) F107 - DAILY F10.7 FLUX FOR PREVIOUS DAY
-            (3) DAILY AP
-            (4) 3 HR AP INDEX FOR CURRENT TIME
-            (5) 3 HR AP INDEX FOR 3 HRS BEFORE CURRENT TIME
-            (6) 3 HR AP INDEX FOR 6 HRS BEFORE CURRENT TIME
-            (7) 3 HR AP INDEX FOR 9 HRS BEFORE CURRENT TIME
-            (8) AVERAGE OF EIGHT 3 HR AP INDICIES FROM 12 TO 33 HRS PRIOR
-                TO CURRENT TIME
-            (9) AVERAGE OF EIGHT 3 HR AP INDICIES FROM 36 TO 59 HRS PRIOR
-                TO CURRENT TIME
-       MASS - MASS NUMBER (ONLY DENSITY FOR SELECTED GAS IS
-                CALCULATED.  MASS 0 IS TEMPERATURE.  MASS 48 FOR ALL.
-
-    Note:  Ut, Local Time, and Longitude are used independently in the
-           model and are not of equal importance for every situation.  
-           For the most physically realistic calculation these three
-           variables should be consistent (STL=SEC/3600+GLONG/15).
-           F107, F107A, and AP effects are not large below 80 km 
-           and these can be set to 150., 150., and 4. respectively.
-
-    OUTPUT:
-       D(1) - HE NUMBER DENSITY(CM-3)
-       D(2) - O NUMBER DENSITY(CM-3)
-       D(3) - N2 NUMBER DENSITY(CM-3)
-       D(4) - O2 NUMBER DENSITY(CM-3)
-       D(5) - AR NUMBER DENSITY(CM-3)                       
-       D(6) - TOTAL MASS DENSITY(GM/CM3)
-       D(7) - H NUMBER DENSITY(CM-3)
-       D(8) - N NUMBER DENSITY(CM-3)
-       T(1) - EXOSPHERIC TEMPERATURE
-       T(2) - TEMPERATURE AT ALT
- */
-/*
-int MSIS90E(int day,double sec,
-	    double alt,double glat,double glon,
-	    double KAP[],
-	    double *rho,double *T)
-{
-  int IYD=day;
-  real SEC=(real)sec;
-  real ALT=(real)alt;
-  real GLAT=(real)glat;
-  real GLONG=(real)glon;
-  real STL=sec/3600.0+GLONG/15.0;
-  real F107A=KAP[0];
-  real F107=KAP[1];
-  int MASS=48;
-  real* AP=newVectorf(7);
-  int k=0,j=2;
-  AP[k++]=KAP[j++];AP[k++]=KAP[j++];AP[k++]=KAP[j++];
-  AP[k++]=KAP[j++];AP[k++]=KAP[j++];AP[k++]=KAP[j++];
-  AP[k++]=KAP[j++];
-  real* D=newVectorf(8);
-  real* Ts=newVectorf(2);
-  gtd6_(&IYD,&SEC,&ALT,&GLAT,&GLONG,&STL,&F107A,&F107,AP,&MASS,D,Ts);
-  *rho=(double)D[5]*1e3;
-  *T=(double)Ts[1];
-  return 0;
-}
-*/
-
 #define KAP_REF {150,22,22,22,22,22,22,22}
 #define FLAGS_ALL {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 int NRLMSISE(int day,double sec,
